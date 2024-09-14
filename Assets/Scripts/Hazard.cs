eaa1ce7ecb82d72ba32d64d1ce891ba8e64d4544
Radi3ntal <ojoryan1@gmail.com>
2024-09-14 12:22:03 -0400

    add teh project

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{       
+    public GameObject background;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        background.GetComponent<GameManager>().moveToCheckPoint();
+    }
+}
